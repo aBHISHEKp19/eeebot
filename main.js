d679fba1a9da9e1c13439329a7629cf07ddefdad
aBHISHEKp <abhishekpoddar4776@gmail.com>
2022-06-26 11:39:51 +0530

    hgu

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-
+const { joinVoiceChannel } = require('@discordjs/voice');
 const client = new Discord.Client({ intents: 32767 });
 require('dotenv').config();
 
@@ -7,6 +7,42 @@ client.once('ready', () => {
     console.log('EEE is here');
     client.user.setActivity('EEE', { type: "WATCHING" })
 });
+
+const prefix = 'eee.';
+
+client.on('messageCreate', message => {
+
+    if (message.content === prefix + 'start') {
+        var interval = setInterval(function () {
+
+            message.channel.send("East or west EEE is the best")
+                .catch(console.error); // add error handling here
+        }, 5 * 1000);
+    }
+    /*
+
+    setTimeout(function () {
+
+        const i = "East or west EEE is the best";
+        
+            message.channel.send(i);
+        
+    }, 5000);*/
+    if (message.content === prefix + 'join') {
+        if (!joinVoiceChannel) return message.channel.send('You need to be in a channel to execute this command!');
+       // if (!args[0]) return message.channel.send("lol");
+
+        const connection = joinVoiceChannel({
+
+            selfDeaf: message.guild.me.voice.setDeaf(false),
+            channelId: message.member.voice.channel.id,
+            guildId: message.guild.id,
+            adapterCreator: message.guild.voiceAdapterCreator,
+
+        })
+    }
+
+})
 
 
 client.login(process.env.TOKEN);
